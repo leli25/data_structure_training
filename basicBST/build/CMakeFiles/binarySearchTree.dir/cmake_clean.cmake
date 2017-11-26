891e4f1e073a6040a6d9e8d09e95dd4282ddca98
LeLi <419235494@qq.com>
2017-11-26 21:10:19 +0800

    add files

---
+++ basicBST/build/CMakeFiles/binarySearchTree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/binarySearchTree.dir/main.cpp.o"
+  "binarySearchTree.pdb"
+  "binarySearchTree"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/binarySearchTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
